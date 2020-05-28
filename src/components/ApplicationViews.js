39a4b7c79f1455e26d19048ac581460d690d2530
Ronald Lankford <rolankford@gmail.com>
2020-05-28 12:40:31 -0500

    book 4 chapter 10

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import React from "react";
 import Home from "./home/Home";
 import AnimalList from "./animal/AnimalList";
@@ -11,11 +11,17 @@ import AnimalForm from './animal/AnimalForm'
 import LocationForm from './location/LocationForm'
 import EmployeeForm from './employee/EmployeeForm'
 import OwnerForm from './owner/OwnerForm'
+import Login from './auth/Login';
 
 
 const ApplicationViews = () => {
+  const isAuthenticated = () => sessionStorage.getItem("credentials") !== null;
   return (
     <>
+      <Route
+        path="/login"
+        component={Login} 
+      />
       <Route
         exact
         path="/"
@@ -27,69 +33,105 @@ const ApplicationViews = () => {
         exact
         path="/animals"
         render={props => {
-          return <AnimalList {...props}/>;
+          if(isAuthenticated()) {
+            return <AnimalList {...props}/>;
+          } else {
+            return <Redirect to="/Login"/>;
+          }
         }}
       />
       <Route
         exact
         path="/locations"
         render={props => {
-          return <LocationList {...props}/>;
+          if(isAuthenticated()){
+            return <LocationList {...props}/>;
+          } else {
+            return <Redirect to="/Login"/>;
+          }
         }}
       />
       <Route
         exact
         path="/employees"
         render={props => {
-          return <EmployeeList {...props}/>;
+          if(isAuthenticated()) {
+            return <EmployeeList {...props}/>;
+          } else {
+            return <Redirect to="/Login"/>;
+          }
         }}
       />
       <Route
         exact
         path="/owners"
         render={props => {
-          return <OwnerList {...props}/>;
+          if(isAuthenticated()) {
+            return <OwnerList {...props}/>;
+          } else {
+            return <Redirect to="/Login"/>;
+          }
         }}
       />
       <Route
         path="/animals/:animalId(\d+)"
         render={props => {
-          return (
-            <AnimalDetail
-              animalId={parseInt(props.match.params.animalId)} {...props} />
-          );
+          if(isAuthenticated()){
+            return (
+              <AnimalDetail
+                animalId={parseInt(props.match.params.animalId)} {...props} />);
+          } else {
+            return <Redirect to="/Login"/>;
+          }
         }}
       />
       <Route 
         path="/locations/:locationId(\d+)" render={
         (props) => {
-          return <LocationDetail 
-            locationId={parseInt(props.match.params.locationId)} {...props}/>;
+          if(isAuthenticated()){
+            return (
+              <LocationDetail locationId={parseInt(props.match.params.locationId)} {...props}/>);
+          } else {
+            return <Redirect to="/Login"/>;
+          }
         }}
       />
       <Route
         path="/animals/new" render={(props) => {
-          return <AnimalForm {...props} />
+          if(isAuthenticated()){
+            return <AnimalForm {...props}/>
+          } else {
+            return <Redirect to="/Login"/>;
+          }
         }}
       />
       <Route
         path="/employees/new" render={(props) => {
-          return <EmployeeForm {...props}
-          />
+          if(isAuthenticated()){
+            return <EmployeeForm {...props}/>;
+          } else {
+            return <Redirect to="/Login"/>;
+          }
         }}
       />
       <Route
         path="/locations/new" render={
         (props) => {
-          return <LocationForm {...props}
-          />
+          if(isAuthenticated()){
+            return <LocationForm {...props}/>;
+          } else {
+            return <Redirect to="/Login"/>;
+          }
         }}
       />
       <Route
         path="/owners/new" render={
         (props) => {
-          return <OwnerForm {...props}
-          />
+          if(isAuthenticated()){
+            return <OwnerForm {...props}/>;
+          } else {
+            return <Redirect to="/Login"/>;
+          }
         }}
       />
     </>
---
+++ src/components/auth/Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const Login = props => {
+  const [credentials, setCredentials] = useState({email: "", password: ""});
+
+  const handleFieldChange = e => {
+    const stateToChange = { ...credentials };
+    stateToChange[e.target.id] = e.target.value;
+    setCredentials(stateToChange);
+  }
+
+  const handleLogin = e => {
+    e.preventDefault();
+    sessionStorage.setItem("credentials", JSON.stringify(credentials));
+    props.history.push("/");
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+      <fieldset>
+        <h3>Please Sign In</h3>
+        <div className="formgrid">
+          <input onChange={handleFieldChange} type="email" id="email" placeholder="Email Address" required="" autoFocus="" />
+          <label htmlFor="inputEmail">Email address</label>
+
+          <input onChange={handleFieldChange} type="password" id="password"  placeholder="Password" required="" />
+          <label htmlFor="inputPassword">Password</label>
+        </div>
+        <button type="submit">Sign In</button>
+      </fieldset>
+    </form>
+  );
+};
+
+export default Login;
