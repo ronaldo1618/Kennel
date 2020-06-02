0b49d71e27c25a5a8405734a16a930427972ac9c
Ronald Lankford <rolankford@gmail.com>
2020-06-02 10:32:22 -0500

    fix details

--- src/components/animal/AnimalDetail.js
+++ src/components/animal/AnimalDetail.js
@@ -1,46 +1,49 @@
-import React, { useState, useEffect } from "react";
-import AnimalManager from "../../modules/AnimalManager";
-import EmployeeManager from '../../modules/EmployeeManager';
-import "./AnimalDetail.css";
+import React, { useState, useEffect } from "react";
+import EmployeeManager from '../../modules/EmployeeManager';
+import AnimalManager from "../../modules/AnimalManager";
+import "./AnimalDetail.css";
 
-const AnimalDetail = (props) => {
-  const [animal, setAnimal] = useState({ name: "", breed: "" });
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    AnimalManager.get(props.animalId).then(animal => {
-      setAnimal({
-        name: animal.name,
-        breed: animal.breed
-      });
-      setIsLoading(false);
-    });
-  }, [props.animalId]);
-
-  const handleDelete = () => {
-    setIsLoading(true);
-    AnimalManager.delete(props.animalId).then(() =>
-      props.history.push(`/animals`)
-    );
-  };
-
-  return (
-    <div className="card">
-      <div className="card-content">
-        <picture>
-          <img src={require("./dog.svg")} alt="My Dog" />
-        </picture>
-        <h3>
-          Name: <span style={{ color: "darkslategrey" }}>{animal.name}</span>
-        </h3>
-        <p>Breed: {animal.breed}</p>
-        <p></p>
-        <button type="button" disabled={isLoading} onClick={handleDelete}>
-          Discharge
-        </button>
-      </div>
-    </div>
-  );
+const AnimalDetail = (props) => {
+  const [animal, setAnimal] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [employees, setEmployees] = useState({});
+  useEffect(() => {
+    AnimalManager.get(props.animalId)
+    .then(animal => {
+      setAnimal({
+        name: animal.name,
+        breed: animal.breed,
+      });
+      return EmployeeManager.get(animal.employeeId)
+    })
+    .then(employee => {
+      setEmployees({name: employee.name})
+      setIsLoading(false);
+    });
+  }, [props.animalId]);
+  const handleDelete = () => {
+    setIsLoading(true);
+    AnimalManager.delete(props.animalId).then(() =>
+      props.history.push(`/animals`)
+    );
+  };
+  return (
+    <div className="card">
+      <div className="card-content">
+        <picture>
+          <img src={require("./dog.svg")} alt="My Dog" />
+        </picture>
+        <h3>
+          Name: 
+          <span style={{ color: "darkslategrey" }}> {animal.name}</span>
+        </h3>
+        <p>Breed: {animal.breed}</p>
+        <p>Caretaker: {employees.name || "not assigned"}</p>
+        <button type="button" disabled={isLoading} onClick={handleDelete}>
+          Discharge
+        </button>
+      </div>
+    </div>
+  );
 };
-
-export default AnimalDetail;
+export default AnimalDetail;
